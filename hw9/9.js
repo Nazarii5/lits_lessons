58331c51e90ba3fbad3f3a3fa984226078fbe37c
nazarii5 <nazkitsaniuk@gmail.com>
2021-12-17 18:05:34 +0200

    hw

---
+++ hw9/9.js
@@ -0,0 +1,51 @@
+var name = 'Nazarii';
+console.log(name)
+
+var surname = "Kitsaniuk";
+console.log(surname)
+
+var age = 25;
+console.log(age)
+
+console.log("Ім'я користувача = " + name)
+console.log("Прізвище користувача = " + surname)
+console.log("Вік = " + age)
+
+console.log(typeof name)
+console.log(typeof surname)
+console.log(typeof age)
+
+var age = age + "";
+console.log(age)
+console.log(typeof age)
+
+
+var call = function () {
+  var name = 'Nazarii';
+console.log(name)
+
+var surname = "Kitsaniuk";
+console.log(surname)
+
+var age = 25;
+console.log(age)
+
+console.log("Ім'я користувача = " + name)
+console.log("Прізвище користувача = " + surname)
+console.log("Вік = " + age)
+
+console.log(typeof name)
+console.log(typeof surname)
+console.log(typeof age)
+
+var age = age + "";
+console.log(age)
+console.log(typeof age)
+  
+}
+
+call();
+call();
+call();
+call();
+call();
