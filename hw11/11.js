e2235450bfae5b6d6bb2c042deae24052d108ec2
nazarii5 <nazkitsaniuk@gmail.com>
2021-12-22 17:11:49 +0200

    hw11

---
+++ hw11/11.js
@@ -0,0 +1,22 @@
+var country1 =  {
+  name: "Ukraine",
+  language: "українська",
+  properties: 1503,
+    population: 126560
+}
+
+var country2 = {
+  name: "USA",
+  language: "English",
+  properties: 1564,
+  population: "13345 people"
+}
+
+var countries = [ country1, country2]
+
+console.log(countries[0].name)
+console.log(countries[0].language)
+console.log(countries[1].name)
+console.log(countries[1].language)
+console.log(countries[1].population)
+
