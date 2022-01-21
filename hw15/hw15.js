84f27c56269b043ce8c7d0cd2a7e7cbe0a72c607
nazarii5 <nazkitsaniuk@gmail.com>
2022-01-21 18:40:52 +0200

    15

---
+++ hw15/hw15.js
@@ -0,0 +1,34 @@
+var countries = [
+  country1 = {
+    name: "Ukraine",
+    language: "українська",
+    properties: 1503,
+    population: 126560,
+    region: "Europe"
+  },
+
+country2 = {
+  name: "UK",
+  language: "English",
+  properties: 123,
+  population: "55634534 people",
+  region: "Asia"
+},
+
+country3 = {
+  name: "Spain",
+  language: "Spanish",
+  properties: 234,
+  population: "34534 people",
+  region: "Europe"
+},
+
+country4 = {
+  name: "China",
+  language: "Chines",
+  properties: 24441,
+  population: "13342342345 people",
+  region:"Asia"
+}];
+
+console.log(countries);
---
+++ hw15/scripts/script.js
@@ -0,0 +1,60 @@
+var countries  = [
+  country1 = {
+    name: "Ukraine",
+    language: "українська",
+    properties: 1503,
+    population: 126560,
+    region: "Europe"
+  },
+
+country2 = {
+  name: "UK",
+  language: "English",
+  properties: 123,
+  population: "55634534 people",
+  region: "Asia"
+},
+
+country3 = {
+  name: "Spain",
+  language: "Spanish",
+  properties: 234,
+  population: "34534 people",
+  region: "Europe"
+},
+
+country4 = {
+  name: "China",
+  language: "Chines",
+  properties: 24441,
+  population: "13342342345 people",
+  region:"Asia"
+}];
+
+console.log(countries);
+
+
+
+
+
+
+var someMain = document.querySelector("main");
+
+var divEle = document.createElement("div");
+divEle.classList.add("some");
+
+const someTextNode = document.createTextNode(
+  
+  ` country1 = {
+  name: "Ukraine",
+  language: "українська",
+  properties: 1503,
+  population: 126560,
+  region: "Europe"
+}
+
+ `);
+divEle.appendChild(someTextNode);
+
+someMain.appendChild(divEle)
+
