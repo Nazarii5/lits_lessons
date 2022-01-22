65dff7987afe0243c5221059df8f0f3604c7d4d0
nazarii5 <nazkitsaniuk@gmail.com>
2022-01-22 18:46:02 +0200

    15 new

--- hw15/scripts/script.js
+++ hw15/scripts/script.js
@@ -51,11 +51,62 @@ const someTextNode = document.createTextNode(
   properties: 1503,
   population: 126560,
   region: "Europe"
-
+  
 
  `);
 divEle.appendChild(someTextNode);
 
 
 someMain.appendChild(divEle)
+
+
+
+
+console.log("-------inner_HTML--------")
+
+someMain.innerHTML = someMain.innerHTML + `<pre class="border">
+<pre >
+Country:
+    name: "Ukraine"
+    language: "українська"
+    properties: 1503
+    population: 126560
+    region: "Europe"
+  
+    </pre>` + `<pre>
+    country:
+      name: "UK",
+      language: "English",
+      properties: 123,
+      population: "55634534 people",
+      region: "Asia"
+      </pre>` +
+    
+    
+  `<pre>
+    country:
+      name: "Spain",
+      language: "Spanish",
+      properties: 234,
+      population: "34534 people",
+      region: "Europe"
+    </pre>` +
+    
+  `<pre>
+    country:
+      name: "China",
+      language: "Chines",
+      properties: 24441,
+      population: "13342342345 people",
+      region:"Asia"
+    </pre>
+    </pre>`;
+  
+const someBorder = document.querySelector(".border");
+
+someBorder.style.display = "flex";
+someBorder.style.border = "2px solid green";
+someBorder.style.background = "yellow";
+someBorder.style.padding="3px" ;
+
 
